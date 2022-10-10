1af665d976eaa25b485943a9782d0bbe01149916
Rohit Kumar <rohitbhai14496@gmail.com>
2022-10-11 00:47:34 +0530

    js file

--- Webdev/simple.js
+++ Webdev/simple.js
@@ -2,3 +2,4 @@ const prompt = require('prompt-sync')();
 var fname = prompt("enter first name: ");
 var lname = prompt("enter last name: ");
 console.log("Your full name :"+fname+lname);
+console.log("Your full name :"+fname+lname);
